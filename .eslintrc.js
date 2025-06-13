447bc7a732bb42d25d86512176df3a15238bc5b2
MEGVII-INC\zoucheng02 <775106512@qq.com>
2025-06-13 16:01:49 +0800

    feat: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
-'use strict'
+'use strict';
 
 module.exports = {
-  extends: 'eslint-config-egg',
+  extends: ['eslint-config-egg', 'prettier'],
   rules: {
-    'comma-dangle': [ 'error', 'never' ],
-    semi: [ 'error', 'never' ],
     'no-unused-vars': 'warn',
     'jsdoc/check-tag-names': [
       'warn',
       {
-        definedTags: [ 'router', 'request', 'response', 'controller' ]
-      }
-    ]
-  }
-}
+        definedTags: ['router', 'request', 'response', 'controller'],
+      },
+    ],
+  },
+};
